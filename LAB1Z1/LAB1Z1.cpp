0a009c5e0673774928a69e5ff4b6ee7744809a75
maks- <maks-@DESKTOP-CR5F1FM>
2021-09-30 21:11:25 +0300

    Добавьте файлы проекта.

---
+++ LAB1Z1/LAB1Z1.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;//задание 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <time.h>
+#include <iostream>
+
+int main() {
+
+	char* locale = setlocale(LC_ALL, "");
+	int a[5], i, b = 0, c = 6;
+
+	srand(time(0));
+
+	for (i = 0; i < 5; i++)
+		a[i] = 1 + rand() % 5;
+	for (i = 0; i < 5; i++)
+		cout << a[i] << " ";
+
+	for (i = 0; i < 5; i++)
+		if (a[i] > b)
+			b = a[i];
+	for (i = 0; i < 5; i++)
+		if (a[i] < c)
+			c = a[i];
+	cout << '\n' << "max = " << b << '\n' << "min = " << c << '\n' << "RAZN = " << b - c;
+
+
+}
